ab922e96b65de00446bcf5b61442ef867a78b3fc
Aleksandr Kuzminsky <aleks@twindb.com>
2017-01-23 22:40:31 -0800

    flake8 fixes

--- proxysql_tools/entities/proxysql.py
+++ proxysql_tools/entities/proxysql.py
@@ -11,14 +11,18 @@ class ProxySQLMySQLBackend(Model):
     """Contains information about MySQL backend"""
     hostgroup_id = IntType(default=0)
 
-    # The name of the host running MySQL. When using unix domain socket, set this to the socket path
+    # The name of the host running MySQL. When using unix domain socket,
+    # set this to the socket path
     hostname = StringType(required=True)
 
-    # The port MySQL is listening on. When using unix domain socket, set this to 0
+    # The port MySQL is listening on. When using unix domain socket,
+    # set this to 0
     port = IntType(default=3306, required=True)
 
-    status = StringType(choices=[BACKEND_STATUS_ONLINE, BACKEND_STATUS_SHUNNED, BACKEND_STATUS_OFFLINE_SOFT,
-                                 BACKEND_STATUS_OFFLINE_HARD], default=BACKEND_STATUS_ONLINE)
+    status = StringType(choices=[BACKEND_STATUS_ONLINE, BACKEND_STATUS_SHUNNED,
+                                 BACKEND_STATUS_OFFLINE_SOFT,
+                                 BACKEND_STATUS_OFFLINE_HARD],
+                        default=BACKEND_STATUS_ONLINE)
 
     weight = IntType(default=1)
     compression = IntType(default=0)
@@ -42,27 +46,33 @@ class ProxySQLMySQLUser(Model):
     active = IntType(choices=[0, 1], default=1)
     use_ssl = IntType(choices=[0, 1], default=0)
 
-    # If there is no matching rule for the queries sent by this user then the traffic is sent to the specified
-    # hostgroup
+    # If there is no matching rule for the queries sent by this user
+    # then the traffic is sent to the specified hostgroup
     default_hostgroup = IntType(default=0)
 
     # The schema to which the connection should change by default
     default_schema = StringType(default='information_schema')
     schema_locked = IntType(choices=[0, 1], default=0)
 
-    # If this is set for the user with which the MySQL client is connecting to ProxySQL (thus a "frontend" user - see
-    # below), transaction started within a hostgroup will remain within that hostgroup regardless of any other rules
+    # If this is set for the user with which the MySQL client is connecting
+    # to ProxySQL (thus a "frontend" user - see
+    # below), transaction started within a hostgroup will remain within
+    # that hostgroup regardless of any other rules
     transaction_persistent = IntType(choices=[0, 1], default=0)
 
-    # If set, it bypasses the query processing layer (rewriting, caching) and passes through the query directly as is
+    # If set, it bypasses the query processing layer (rewriting, caching)
+    # and passes through the query directly as is
     # to the backend server
     fast_forward = IntType(choices=[0, 1], default=0)
 
-    # Note, currently all users need both "frontend" and "backend" set to 1. If set to 1, this (username, password)
-    # pair is used for authenticating to the MySQL servers against any hostgroup
+    # Note, currently all users need both "frontend" and "backend" set to 1.
+    # If set to 1, this (username, password)
+    # pair is used for authenticating to the MySQL servers against
+    # any hostgroup
     backend = IntType(choices=[0, 1], default=1)
 
-    # If set to 1, this (username, password) pair is used for authenticating to the ProxySQL instance
+    # If set to 1, this (username, password) pair is used for authenticating
+    # to the ProxySQL instance
     frontend = IntType(choices=[0, 1], default=1)
 
     max_connections = IntType(default=10000)
