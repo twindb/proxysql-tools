be6fa895aa6c0a677ad51e18846730dabdf637bd
Aleksandr Kuzminsky <aleks@twindb.com>
2017-04-03 23:09:23 -0700

    Bump version: 0.2.9 → 0.2.10

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ test_requirements = [str(ir.req) for ir in
 
 setup(
     name='proxysql_tools',
-    version='0.2.9',
+    version='0.2.10',
     description="ProxySQL Tools",
     long_description=readme + '\n\n' + history,
     author="TwinDB Development Team",
