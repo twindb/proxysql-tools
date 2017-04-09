333774463051d73c1d43426cbe698113f6c755ac
Ovais Tariq <ovaistariq@gmail.com>
2017-04-08 19:05:28 -0700

    Add unit test to test deregister_unhealthy_backends

--- tests/unit/test_unit_galera.py
+++ tests/unit/test_unit_galera.py
@@ -1,7 +1,21 @@
 import pytest
 
-from proxysql_tools.entities.galera import GaleraConfig
-from proxysql_tools.galera import fetch_galera_manager
+from doubles import allow
+
+from proxysql_tools.entities.galera import (
+    GaleraConfig,
+    GaleraNode,
+    LOCAL_STATE_SYNCED
+)
+from proxysql_tools.entities.proxysql import (
+    ProxySQLMySQLBackend,
+    BACKEND_STATUS_ONLINE
+)
+from proxysql_tools.galera import (
+    fetch_galera_manager,
+    deregister_unhealthy_backends
+)
+from proxysql_tools.managers.proxysql_manager import ProxySQLManager
 
 
 def test__fetch_galera_manager_can_create_galera_manager_object_with_valid_config(mocker):  # NOQA
@@ -32,3 +46,53 @@ def test__fetch_galera_manager_raises_exception_on_incorrect_cluster_host_config
 
     with pytest.raises(ValueError):
         fetch_galera_manager(galera_config)
+
+
+def test__deregister_unhealthy_backends_deregisters_unhealthy_backends():
+    healthy_node = GaleraNode({
+        'host': '192.168.10.1',
+        'port': 3306,
+        'local_state': 4
+    })
+
+    unhealthy_node = GaleraNode({
+        'host': '192.168.10.2',
+        'port': 3306,
+        'local_state': 2
+    })
+
+    healthy_backend = ProxySQLMySQLBackend({
+        'status': BACKEND_STATUS_ONLINE,
+        'hostname': '192.168.10.1',
+        'port': 3306
+    })
+
+    unhealthy_backend = ProxySQLMySQLBackend({
+        'status': BACKEND_STATUS_ONLINE,
+        'hostname': '192.168.10.2',
+        'port': 3306
+    })
+
+    galera_nodes = [healthy_node, unhealthy_node]
+    backends = [healthy_backend, unhealthy_backend]
+    hostgroup_id = 11
+
+    proxysql_man = ProxySQLManager(host='127.0.0.1', port=6032, user='username',
+                                   password='password')
+
+    (allow(proxysql_man)
+        .fetch_backends
+        .with_args(hostgroup_id)
+        .and_return(backends))
+
+    (allow(proxysql_man)
+        .deregister_backend
+        .with_args(hostgroup_id, unhealthy_backend.hostname, unhealthy_backend.port)  # NOQA
+        .and_return(True))
+
+    backends_list = deregister_unhealthy_backends(proxysql_man, galera_nodes,
+                                                  hostgroup_id,
+                                                  [LOCAL_STATE_SYNCED])
+
+    assert len(backends_list) == 1
+    assert backends_list.pop() == healthy_backend
