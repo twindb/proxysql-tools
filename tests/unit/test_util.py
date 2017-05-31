16886002c8fec0d6a197c5d8187792eed16eb301
Maksym Kryva <mkryva@opendesign.com>
2017-05-31 18:10:06 +0300

    Fixed lint + implement mock for unit tests

--- tests/unit/test_util.py
+++ tests/unit/test_util.py
@@ -1,1 +1,7 @@
-from proxysql_tools.util import bug1258464
+import pytest
+from doubles import allow, expect
+from proxysql_tools.util.bug1258464 import bug1258464
+
+
+def test__bug1258464killer_when_count_of_COMMIT_less_then_100(mocker):
+    pass
