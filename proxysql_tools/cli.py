6aca0585d7c5eaa42ba7e3e6e25b43770afc3f3e
mkryva <maksym.kryva@gmail.com>
2017-06-01 15:00:59 +0300

    Implmeneted tests for bug killer

--- proxysql_tools/cli.py
+++ proxysql_tools/cli.py
@@ -109,7 +109,7 @@ def register(cfg):
 @galera.command()
 @PASS_CFG
 @click.option('--default-file', help='Path to my.cnf with custom settings')
-def bug1258464killer(cfg,default_file):
+def bug1258464killer(default_file):
     if os.path.isfile(default_file):
         bug1258464(default_file)
     else:
