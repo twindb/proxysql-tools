dd82e779996affdae728b25f01ee5c50e69c64dd
mkryva <maksym.kryva@gmail.com>
2017-06-14 11:42:53 +0300

    Added integration test for writer

--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -197,3 +197,79 @@ def test__galera_register_shutdowned_reader_is_degistered(percona_xtradb_cluster
     finally:
         connection.close()
 
+def test__galera_register_shutdowned_writer_is_deregistered(percona_xtradb_cluster_three_node,
+                                                   proxysql_instance,
+                                                   tmpdir):
+    wait_for_cluster_nodes_to_become_healthy(percona_xtradb_cluster_three_node)
+    hostgroup_writer = 10
+    hostgroup_reader = 11
+
+    backend_unreg = ProxySQLMySQLBackend(hostname=percona_xtradb_cluster_three_node[0]['ip'],
+                                port=percona_xtradb_cluster_three_node[0]['mysql_port'],
+                                hostgroup_id=hostgroup_writer
+    )
+    proxysql_instance.register_backend(backend_unreg)
+
+    backend = ProxySQLMySQLBackend(hostname=percona_xtradb_cluster_three_node[1]['ip'],
+                                port=percona_xtradb_cluster_three_node[1]['mysql_port'],
+                                hostgroup_id=hostgroup_reader
+    )
+    proxysql_instance.register_backend(backend)
+
+    backend = ProxySQLMySQLBackend(hostname=percona_xtradb_cluster_three_node[2]['ip'],
+                                port=percona_xtradb_cluster_three_node[2]['mysql_port'],
+                                hostgroup_id=hostgroup_reader
+    )
+    proxysql_instance.register_backend(backend)
+
+    blacklist = '{}:3306'.format(percona_xtradb_cluster_three_node[2]['ip'])
+    nodes = [percona_xtradb_cluster_three_node[0]['ip'] + ':3306',
+             percona_xtradb_cluster_three_node[1]['ip'] + ':3306',
+             percona_xtradb_cluster_three_node[2]['ip'] + ':3306'
+             ]
+
+    shutdown_container(percona_xtradb_cluster_three_node[0]['id'])
+    config = proxysql_tools_config_2(proxysql_instance,
+                                     nodes,
+                                     'root', 'r00t', hostgroup_writer,
+                                     hostgroup_reader,
+                                     blacklist, 'monitor',
+                                     'monitor')
+    config_file = str(tmpdir.join('proxysql-tool.cfg'))
+    with open(config_file, 'w') as fh:
+        config.write(fh)
+        proxysql_tools.LOG.debug('proxysql-tools config: \n%s', config)
+    runner = CliRunner()
+    result = runner.invoke(main, ['--config', config_file, 'galera', 'register'])
+    assert result.exit_code == 0
+
+    connection = pymysql.connect(host=proxysql_instance.host, port=proxysql_instance.port,
+                               user=proxysql_instance.user, passwd=proxysql_instance.password,
+                               connect_timeout=20,
+                               cursorclass=DictCursor)
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute('SELECT `hostgroup_id`, `hostname`, '
+                              '`port`'
+                              ' FROM `mysql_servers`'
+                              ' WHERE hostgroup_id = %s '
+                              ' AND `hostname` = %s '
+                              ' AND `port` = %s',
+                              (
+                                  backend_unreg.hostgroup_id,
+                                  backend_unreg.hostname,
+                                  backend_unreg.port
+                              ))
+            assert cursor.fetchall() == ()
+
+            cursor.execute('SELECT `hostgroup_id`, `hostname`, '
+                              '`port`'
+                              ' FROM `mysql_servers`'
+                              ' WHERE hostgroup_id = %s ',
+                              (
+                                  backend_unreg.hostgroup_id,
+                              ))
+            assert cursor.fetchall() != ()
+
+    finally:
+        connection.close()
