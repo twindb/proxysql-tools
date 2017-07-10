c7a9edbaf88f1ca4632bdd27c8230b4a2e3f4977
Aleksandr Kuzminsky <aleks@twindb.com>
2017-07-09 23:04:33 -0700

    Implement set_desync and set_sync sub-commands

--- proxysql_tools/cli.py
+++ proxysql_tools/cli.py
@@ -4,13 +4,15 @@ import os
 from ConfigParser import ConfigParser, NoOptionError, NoSectionError
 
 import click
-from pymysql import OperationalError
+from pymysql import MySQLError
 
 from proxysql_tools import setup_logging, LOG, __version__, OPTIONS_MAPPING
 from proxysql_tools.aws.aws import aws_notify_master
 from proxysql_tools.cli_entrypoint.galera import galera_register
-from proxysql_tools.galera.server import server_status
+from proxysql_tools.galera.server import server_status, \
+    server_set_wsrep_desync
 from proxysql_tools.galera.user import get_users
+from proxysql_tools.proxysql.exceptions import ProxySQLBackendNotFound
 from proxysql_tools.proxysql.proxysql import ProxySQL
 from proxysql_tools.util.bug1258464 import bug1258464
 
@@ -99,7 +101,7 @@ def register(cfg):
         LOG.error('Failed to parse config: %s', err)
         exit(1)
 
-    except OperationalError as err:
+    except MySQLError as err:
         LOG.error('Failed to talk to database: %s', err)
 
 
@@ -140,8 +142,53 @@ def status(cfg):
         LOG.error('Failed to parse config: %s', err)
         exit(1)
 
-    except OperationalError as err:
+    except MySQLError as err:
         LOG.error('Failed to talk to database: %s', err)
+
+
+@server.command()
+@click.argument('ip_address', required=True)
+@click.argument('port', required=False, type=int, default=3306)
+@PASS_CFG
+def set_desync(cfg, ip_address, port):
+    """Set server to desync state."""
+
+    try:
+        server_set_wsrep_desync(cfg, ip_address, port, wsrep_desync='ON')
+    except NotImplementedError as err:
+        LOG.error(err)
+        exit(1)
+    except (NoOptionError, NoSectionError) as err:
+        LOG.error('Failed to parse config: %s', err)
+        exit(1)
+
+    except MySQLError as err:
+        LOG.error('Failed to talk to database: %s', err)
+    except ProxySQLBackendNotFound as err:
+        LOG.error(err)
+
+
+@server.command()
+@click.argument('ip_address', required=True)
+@click.argument('port', required=False, type=int, default=3306)
+@PASS_CFG
+def set_sync(cfg, ip_address, port):
+    """Set server to sync state."""
+
+    try:
+        server_set_wsrep_desync(cfg, ip_address, port, wsrep_desync='OFF')
+    except NotImplementedError as err:
+        LOG.error(err)
+        exit(1)
+    except (NoOptionError, NoSectionError) as err:
+        LOG.error('Failed to parse config: %s', err)
+        exit(1)
+
+    except MySQLError as err:
+        LOG.error('Failed to talk to database: %s', err)
+    except ProxySQLBackendNotFound as err:
+        LOG.error(err)
+
 
 @galera.group()
 def user():
