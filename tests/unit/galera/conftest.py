d3e05c6cc4d9679b0ba6623f5cbba7b734e6541d
mkryva <maksym.kryva@gmail.com>
2017-07-11 18:40:30 +0300

    Added test for server_status

--- tests/unit/galera/conftest.py
+++ tests/unit/galera/conftest.py
@@ -1,8 +1,19 @@
 import pytest
 
 from proxysql_tools.galera.galera_node import GaleraNode
+from proxysql_tools.proxysql.proxysql import ProxySQL
+from tests.integration.library import proxysql_tools_config
 
 
 @pytest.fixture
 def galera_node():
     return GaleraNode('foo', port=1234, user='bar', password='xyz')
+
+
+@pytest.fixture
+def config():
+    proxysql_instance = ProxySQL()
+    data = proxysql_tools_config(proxysql_instance, '127.0.0.1', '3306',
+                                 'user', 'pass', 10, 11, 'monitor',
+                                 'monitor')
+    return data
--- tests/unit/galera/test_galera_server.py
+++ tests/unit/galera/test_galera_server.py
@@ -1,1 +1,8 @@
 import mock
+from proxysql_tools.galera.server import server_status, server_set_wsrep_desync
+from proxysql_tools.proxysql.proxysql import ProxySQL
+
+
+@mock.patch.object(ProxySQL, 'find_backends')
+def test_server_status(config):
+    server_status(config)
