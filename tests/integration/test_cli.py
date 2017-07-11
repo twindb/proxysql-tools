cd345514ed2402a329af24304e361833bdbc2d2e
mkryva <maksym.kryva@gmail.com>
2017-07-11 17:38:08 +0300

    Added status integration test

--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -519,3 +519,49 @@ def test__galera_register_writer_node_is_reader_when_readers_list_is_empty(perco
     finally:
         connection.close()
 
+
+def test__server_status(percona_xtradb_cluster_three_node,
+                        proxysql_instance, tmpdir):
+    wait_for_cluster_nodes_to_become_healthy(percona_xtradb_cluster_three_node)
+    hostgroup_writer = 10
+    hostgroup_reader = 11
+
+    rw_map = {
+        0: hostgroup_writer,
+        1: hostgroup_reader,
+        2: hostgroup_reader
+    }
+    for i in xrange(3):
+        backend = ProxySQLMySQLBackend(
+            hostname=percona_xtradb_cluster_three_node[i]['ip'],
+            port=percona_xtradb_cluster_three_node[0]['mysql_port'],
+            hostgroup_id=rw_map[i]
+        )
+        proxysql_instance.register_backend(backend)
+
+    blacklist = '{}:3306'.format(percona_xtradb_cluster_three_node[2]['ip'])
+    nodes = [
+        percona_xtradb_cluster_three_node[0]['ip'] + ':3306',
+        percona_xtradb_cluster_three_node[1]['ip'] + ':3306',
+        percona_xtradb_cluster_three_node[2]['ip'] + ':3306'
+    ]
+    config = proxysql_tools_config_2(proxysql_instance,
+                                     nodes,
+                                     'root', 'r00t', hostgroup_writer,
+                                     hostgroup_reader,
+                                     blacklist, 'monitor',
+                                     'monitor')
+    config_file = str(tmpdir.join('proxysql-tool.cfg'))
+    with open(config_file, 'w') as fh:
+        config.write(fh)
+        proxysql_tools.LOG.debug('proxysql-tools config: \n%s', config)
+    runner = CliRunner()
+    result = runner.invoke(main,
+                           ['--config', config_file, 'galera', 'register']
+                           )
+    assert result.exit_code == 0
+
+    result = runner.invoke(main,
+                           ['--config', config_file, 'galera', 'server', 'status']
+                           )
+    assert result.exit_code == 0
