d73f352003886a208f1acf67c52ea3ddbf6cdc0d
mkryva <maksym.kryva@gmail.com>
2017-07-11 19:24:32 +0300

    Added tests for galera server

--- tests/unit/galera/conftest.py
+++ tests/unit/galera/conftest.py
@@ -1,13 +1,18 @@
 import pytest
 
 from proxysql_tools.galera.galera_node import GaleraNode
-from proxysql_tools.proxysql.proxysql import ProxySQL
+from proxysql_tools.proxysql.proxysql import ProxySQL, ProxySQLMySQLBackend
 from tests.integration.library import proxysql_tools_config
 
 
 @pytest.fixture
 def galera_node():
     return GaleraNode('foo', port=1234, user='bar', password='xyz')
+
+
+@pytest.fixture
+def proxysql_mysql_backend():
+    return ProxySQLMySQLBackend('127.0.0.1')
 
 
 @pytest.fixture
