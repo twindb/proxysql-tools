f182b0c83533b7ffd073f017a1f6cf4556fdbb48
mkryva <maksym.kryva@gmail.com>
2017-07-12 21:06:17 +0300

    Added integration tests for delete user

--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -756,7 +756,6 @@ def test__galera_user_create_using_options(proxysql_instance, tmpdir):
         connection.close()
 
 
-
 def test__galera_user_set_password_if_user_is_exist(proxysql_instance, tmpdir):
     config = proxysql_tools_config(proxysql_instance, '127.0.0.1', '3306',
                                    'user', 'pass', 10, 11, 'monitor',
@@ -840,3 +839,53 @@ def test__galera_user_set_password_if_user_is_not_exist(proxysql_instance, tmpdi
     assert result.exit_code == 1
 
 
+def test__galera_user_delete(proxysql_instance, tmpdir):
+    config = proxysql_tools_config(proxysql_instance, '127.0.0.1', '3306',
+                                   'user', 'pass', 10, 11, 'monitor',
+                                   'monitor')
+    config_file = str(tmpdir.join('proxysql-tool.cfg'))
+    with open(config_file, 'w') as fh:
+        config.write(fh)
+        proxysql_tools.LOG.debug('proxysql-tools config: \n%s', config)
+    runner = CliRunner()
+    result = runner.invoke(main,
+                           ['--config', config_file, 'galera', 'user', 'create', 'foo']
+                           )
+    assert result.exit_code == 0
+
+    connection = pymysql.connect(
+        host=proxysql_instance.host,
+        port=proxysql_instance.port,
+        user=proxysql_instance.user,
+        passwd=proxysql_instance.password,
+        connect_timeout=20,
+        cursorclass=DictCursor)
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM mysql_users WHERE username = '{username}'"
+                           .format(username='foo'))
+            assert cursor.fetchall() != ()
+
+    finally:
+        connection.close()
+    result = runner.invoke(main,
+                           ['--config', config_file, 'galera', 'user', 'delete', 'foo']
+                           )
+
+    assert result.exit_code == 0
+
+    connection = pymysql.connect(
+        host=proxysql_instance.host,
+        port=proxysql_instance.port,
+        user=proxysql_instance.user,
+        passwd=proxysql_instance.password,
+        connect_timeout=20,
+        cursorclass=DictCursor)
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM mysql_users WHERE username = '{username}'"
+                           .format(username='foo'))
+            assert cursor.fetchall() == ()
+
+    finally:
+        connection.close()
