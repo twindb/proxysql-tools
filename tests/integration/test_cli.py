989a87baab5795e1d9c37cecb9885bee2716c330
mkryva <maksym.kryva@gmail.com>
2017-07-12 17:21:32 +0300

    Added integration tests for create_user and set_password

--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -3,7 +3,7 @@ from click.testing import CliRunner
 import proxysql_tools
 from proxysql_tools.cli import main
 from proxysql_tools.galera.galera_node import GaleraNode
-from proxysql_tools.proxysql.proxysql import ProxySQLMySQLBackend, BackendStatus
+from proxysql_tools.proxysql.proxysql import ProxySQLMySQLBackend, BackendStatus, ProxySQLMySQLUser
 from tests.integration.library import proxysql_tools_config, \
     wait_for_cluster_nodes_to_become_healthy, proxysql_tools_config_2, \
     shutdown_container
@@ -618,7 +618,7 @@ def test__galera_server_set_desync(percona_xtradb_cluster_three_node,
                            ['--config', config_file, 'galera', 'register']
                            )
     assert result.exit_code == 0
-    
+
     connection = pymysql.connect(
         host=proxysql_instance.host,
         port=proxysql_instance.port,
@@ -723,4 +723,120 @@ def test__galera_server_set_sync(percona_xtradb_cluster_three_node,
             assert row['status'] == BackendStatus.online
     finally:
         connection.close()
+
+
+def test__galera_user_create_using_options(proxysql_instance, tmpdir):
+    config = proxysql_tools_config(proxysql_instance, '127.0.0.1', '3306',
+                                   'user', 'pass', 10, 11, 'monitor',
+                                   'monitor')
+    config_file = str(tmpdir.join('proxysql-tool.cfg'))
+    with open(config_file, 'w') as fh:
+        config.write(fh)
+        proxysql_tools.LOG.debug('proxysql-tools config: \n%s', config)
+    runner = CliRunner()
+    result = runner.invoke(main,
+                           ['--config', config_file, 'galera', 'user', 'create', 'foo']
+                           )
+    assert result.exit_code == 0
+
+    connection = pymysql.connect(
+        host=proxysql_instance.host,
+        port=proxysql_instance.port,
+        user=proxysql_instance.user,
+        passwd=proxysql_instance.password,
+        connect_timeout=20,
+        cursorclass=DictCursor)
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM mysql_users WHERE username = '{username}'"
+                              .format(username='foo'))
+            assert cursor.fetchall() != ()
+
+    finally:
+        connection.close()
+
+
+
+def test__galera_user_set_password_if_user_is_exist(proxysql_instance, tmpdir):
+    config = proxysql_tools_config(proxysql_instance, '127.0.0.1', '3306',
+                                   'user', 'pass', 10, 11, 'monitor',
+                                   'monitor')
+    config_file = str(tmpdir.join('proxysql-tool.cfg'))
+    with open(config_file, 'w') as fh:
+        config.write(fh)
+        proxysql_tools.LOG.debug('proxysql-tools config: \n%s', config)
+    runner = CliRunner()
+    result = runner.invoke(main,
+                           ['--config', config_file, 'galera', 'user', 'create', 'foo']
+                           )
+    assert result.exit_code == 0
+
+    connection = pymysql.connect(
+        host=proxysql_instance.host,
+        port=proxysql_instance.port,
+        user=proxysql_instance.user,
+        passwd=proxysql_instance.password,
+        connect_timeout=20,
+        cursorclass=DictCursor)
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM mysql_users WHERE username = '{username}'"
+                           .format(username='foo'))
+            assert cursor.fetchall() != ()
+
+    finally:
+        connection.close()
+
+    result = runner.invoke(main,
+                           ['--config', config_file, 'galera', 'user', 'set_password', 'foo'], input='test'
+                           )
+
+    assert result.exit_code == 0
+
+    connection = pymysql.connect(
+        host=proxysql_instance.host,
+        port=proxysql_instance.port,
+        user=proxysql_instance.user,
+        passwd=proxysql_instance.password,
+        connect_timeout=20,
+        cursorclass=DictCursor)
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM mysql_users WHERE username = '{username}'"
+                           .format(username='foo'))
+
+            row = cursor.fetchall()[0]
+            user = ProxySQLMySQLUser(user=row['username'],
+                                     password=row['password'],
+                                     active=row['active'],
+                                     use_ssl=row['use_ssl'],
+                                     default_hostgroup=row['default_hostgroup'],
+                                     default_schema=row['default_schema'],
+                                     schema_locked=row['schema_locked'],
+                                     transaction_persistent=row['transaction_persistent'],
+                                     fast_forward=row['fast_forward'],
+                                     backend=row['backend'],
+                                     frontend=row['frontend'],
+                                     max_connections=row['max_connections'])
+            assert user.password == 'test'
+    finally:
+        connection.close()
+
+
+def test__galera_user_set_password_if_user_is_not_exist(proxysql_instance, tmpdir):
+    config = proxysql_tools_config(proxysql_instance, '127.0.0.1', '3306',
+                                   'user', 'pass', 10, 11, 'monitor',
+                                   'monitor')
+    config_file = str(tmpdir.join('proxysql-tool.cfg'))
+    with open(config_file, 'w') as fh:
+        config.write(fh)
+        proxysql_tools.LOG.debug('proxysql-tools config: \n%s', config)
+    runner = CliRunner()
+
+    result = runner.invoke(main,
+                           ['--config', config_file, 'galera', 'user', 'set_password', 'foo'], input='test'
+                           )
+
+    assert result.exit_code == 1
+
 
