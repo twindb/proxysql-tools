06a3d5400aa0d362d4e598055e8c7e70ee0bb594
mkryva <maksym.kryva@gmail.com>
2017-07-31 23:09:16 +0300

    Added exception handler

--- proxysql_tools/cli.py
+++ proxysql_tools/cli.py
@@ -147,9 +147,11 @@ def status(cfg):
     except (NoOptionError, NoSectionError) as err:
         LOG.error('Failed to parse config: %s', err)
         exit(1)
-
     except MySQLError as err:
         LOG.error('Failed to talk to database: %s', err)
+        exit(1)
+    except ProxySQLBackendNotFound as err:
+        LOG.error(err)
 
 
 @server.command()
