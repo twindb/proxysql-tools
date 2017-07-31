011cf9d36af5570b0e2536b96376bc455414b2de
mkryva <maksym.kryva@gmail.com>
2017-07-31 21:02:12 +0300

    try to fix imports

--- proxysql_tools/util/__init__.py
+++ proxysql_tools/util/__init__.py
@@ -150,10 +150,3 @@ def parse_user_arguments(args):
             raise ValueError('Unexpected argument: %s', arg)
         i = i+1
     return result
-
-
-def get_backend_comment(role, status):
-    comment = {}
-    comment['role'] = role
-    comment['admin_status'] = status
-    return json.dumps(comment)
