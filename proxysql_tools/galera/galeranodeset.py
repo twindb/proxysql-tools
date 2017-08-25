d9bdf3b9e737c5ae7de1dcfa4feb44e193f69e5c
mkryva <maksym.kryva@gmail.com>
2017-08-25 11:36:42 +0300

    Added BackendState interface

---
+++ proxysql_tools/galera/galeranodeset.py
@@ -0,0 +1,57 @@
+from proxysql_tools.galera.exceptions import GaleraClusterNodeNotFound
+from proxysql_tools.galera.galera_node import GaleraNode, GaleraNodeState
+from proxysql_tools.proxysql.backendset import BackendSet
+
+
+class GaleraNodeSet(BackendSet):
+    """Set for galera nodes"""
+
+    def __contains__(self, item):
+        if isinstance(item, GaleraNode):
+            return item in self._backend_list
+        elif isinstance(item, GaleraNodeSet):
+            for backend in item:
+                if backend not in self:
+                    return False
+            return True
+
+        return False
+
+    def find(self, host=None, port=3306, state=None):
+        """
+        Find node by host and port
+
+        :param host: Hostname of backend
+        :param port: Port of backend
+        :param state: State of node
+        :return: Return found node
+        :raises: GaleraClusterNodeNotFound
+        """
+
+        if host:
+            needle = GaleraNode(host, port)
+            for node in self._backend_list:
+                if needle == node:
+                    return node
+            raise GaleraClusterNodeNotFound('Backend %s not found' % needle)
+
+        if state:
+            nodes = []
+            for node in self._backend_list:
+                if node.wsrep_local_state == state:
+                    nodes.append(node)
+            if nodes:
+                return nodes
+            raise GaleraClusterNodeNotFound('Nodes with state %d not found' % state)
+
+    def remove(self, backend):
+        """Remove node from the set
+
+        :param backend: Node to remove.
+        :type backend: GaleraNode
+        :raise GaleraClusterNodeNotFound: if node is not in the set
+        """
+        try:
+            self._backend_list.remove(backend)
+        except ValueError as err:
+            raise GaleraClusterNodeNotFound(err)
---
+++ proxysql_tools/proxysql/backendset.py
@@ -0,0 +1,66 @@
+from abc import abstractmethod
+
+
+class BackendSet(object):
+    """Base class for sets of nodes and backends"""
+
+    def __init__(self):
+        self._backend_list = []
+        self._set_iterator = 0
+
+    def __len__(self):
+        return len(self._backend_list)
+
+    @abstractmethod
+    def __contains__(self, item):
+        pass
+
+    def __eq__(self, other):
+        return other == self._backend_list
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        """Return next Backend"""
+        try:
+            backend = self._backend_list[self._set_iterator]
+            self._set_iterator += 1
+            return backend
+        except IndexError:
+            self._set_iterator = 0
+            raise StopIteration()
+
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            return self._backend_list[key]
+
+    def add_set(self, backend_set):
+        """
+        Add iterable object to list
+
+        :param backend_set:
+        :type backend_set:
+        """
+        for backend in backend_set:
+            self._backend_list.append(backend)
+
+    def add(self, backend):
+        """
+        Add backend
+
+        :param backend: Backend
+        """
+        self._backend_list.append(backend)
+
+    @abstractmethod
+    def remove(self, backend):
+        """Remove backend from the set
+
+        :param backend: Backend to remove.
+        :type backend: ProxySQLMySQLBackend
+        :raise ProxySQLBackendNotFound: if backend is not in the set
+        """
