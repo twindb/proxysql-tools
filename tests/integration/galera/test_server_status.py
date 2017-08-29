567d17cc59ff0b6dc52f99e0d69cf52353979f98
mkryva <maksym.kryva@gmail.com>
2017-08-29 19:02:01 +0300

    Added tests that run galera register with clean mysql_servers

--- tests/integration/galera/test_server_status.py
+++ tests/integration/galera/test_server_status.py
@@ -51,3 +51,37 @@ def test__galera_server_status(percona_xtradb_cluster_three_node,
                            ['--config', config_file, 'galera', 'server', 'status']
                            )
     assert result.exit_code == 0
+
+
+def test__galera_server_status_with_empty_mysql_servers(percona_xtradb_cluster_three_node,
+                                                        proxysql_instance, tmpdir):
+    wait_for_cluster_nodes_to_become_healthy(percona_xtradb_cluster_three_node)
+    hostgroup_writer = 10
+    hostgroup_reader = 11
+
+    blacklist = '{}:3306'.format(percona_xtradb_cluster_three_node[2]['ip'])
+    nodes = [
+        percona_xtradb_cluster_three_node[0]['ip'] + ':3306',
+        percona_xtradb_cluster_three_node[1]['ip'] + ':3306',
+        percona_xtradb_cluster_three_node[2]['ip'] + ':3306'
+    ]
+    config = proxysql_tools_config_2(proxysql_instance,
+                                     nodes,
+                                     'root', 'r00t', hostgroup_writer,
+                                     hostgroup_reader,
+                                     blacklist, 'monitor',
+                                     'monitor')
+    config_file = str(tmpdir.join('proxysql-tool.cfg'))
+    with open(config_file, 'w') as fh:
+        config.write(fh)
+        proxysql_tools.LOG.debug('proxysql-tools config: \n%s', config)
+    runner = CliRunner()
+    result = runner.invoke(main,
+                           ['--config', config_file, 'galera', 'register']
+                           )
+    assert result.exit_code == 0
+
+    result = runner.invoke(main,
+                           ['--config', config_file, 'galera', 'server', 'status']
+                           )
+    assert result.exit_code == 0
--- tests/integration/galera/test_set_offline.py
+++ tests/integration/galera/test_set_offline.py
@@ -89,6 +89,72 @@ def test__galera_server_set_offline_reader_if_backends_are_registered(percona_xt
         connection.close()
 
 
+def test__galera_server_set_offline_reader_if_backends_are_registered_with_empty_mysql_servers(percona_xtradb_cluster_three_node,
+                                                                                               proxysql_instance,
+                                                                                               tmpdir):
+
+    wait_for_cluster_nodes_to_become_healthy(percona_xtradb_cluster_three_node)
+    hostgroup_writer = 10
+    hostgroup_reader = 11
+
+    blacklist = '{}:3306'.format(percona_xtradb_cluster_three_node[2]['ip'])
+    nodes = [
+        percona_xtradb_cluster_three_node[0]['ip'] + ':3306',
+        percona_xtradb_cluster_three_node[1]['ip'] + ':3306',
+        percona_xtradb_cluster_three_node[2]['ip'] + ':3306'
+    ]
+    config = proxysql_tools_config_2(proxysql_instance,
+                                     nodes,
+                                     'root', 'r00t', hostgroup_writer,
+                                     hostgroup_reader,
+                                     blacklist, 'monitor',
+                                     'monitor')
+    config_file = str(tmpdir.join('proxysql-tool.cfg'))
+    with open(config_file, 'w') as fh:
+        config.write(fh)
+        proxysql_tools.LOG.debug('proxysql-tools config: \n%s', config)
+    runner = CliRunner()
+    result = runner.invoke(main,
+                           ['--config', config_file, 'galera', 'register']
+                           )
+    assert result.exit_code == 0
+    result = runner.invoke(main,
+                           ['--config', config_file, 'galera', 'server', 'set_offline', percona_xtradb_cluster_three_node[2]['ip'], '3306']
+                           )
+    assert result.exit_code == 0
+
+    result = runner.invoke(main,
+                           ['--config', config_file, 'galera', 'register']
+                           )
+    assert result.exit_code == 0
+
+    connection = pymysql.connect(
+        host=proxysql_instance.host,
+        port=proxysql_instance.port,
+        user=proxysql_instance.user,
+        passwd=proxysql_instance.password,
+        connect_timeout=20,
+        cursorclass=DictCursor)
+
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute('SELECT `hostgroup_id`, `hostname`, '
+                           '`port`, `status`, `weight`, `compression`, '
+                           '`max_connections`, `max_replication_lag`, '
+                           '`use_ssl`, `max_latency_ms`, `comment`'
+                           ' FROM `mysql_servers`'
+                           ' WHERE hostgroup_id = %s'
+                           ' AND hostname = %s',
+                           (
+                               hostgroup_reader,
+                               percona_xtradb_cluster_three_node[2]['ip']
+                           )
+                           )
+            row = cursor.fetchall()[0]
+            assert row['status'] == BackendStatus.offline_hard
+    finally:
+        connection.close()
+
 def test__galera_server_set_offline_writer_if_backends_are_registered(percona_xtradb_cluster_three_node,
                                                    proxysql_instance,
                                                    tmpdir):
@@ -109,6 +175,73 @@ def test__galera_server_set_offline_writer_if_backends_are_registered(percona_xt
             hostgroup_id=rw_map[i]
         )
         proxysql_instance.register_backend(backend)
+
+    blacklist = '{}:3306'.format(percona_xtradb_cluster_three_node[1]['ip'])
+    nodes = [
+        percona_xtradb_cluster_three_node[0]['ip'] + ':3306',
+        percona_xtradb_cluster_three_node[1]['ip'] + ':3306',
+        percona_xtradb_cluster_three_node[2]['ip'] + ':3306'
+    ]
+    config = proxysql_tools_config_2(proxysql_instance,
+                                     nodes,
+                                     'root', 'r00t', hostgroup_writer,
+                                     hostgroup_reader,
+                                     blacklist, 'monitor',
+                                     'monitor')
+    config_file = str(tmpdir.join('proxysql-tool.cfg'))
+    with open(config_file, 'w') as fh:
+        config.write(fh)
+        proxysql_tools.LOG.debug('proxysql-tools config: \n%s', config)
+    runner = CliRunner()
+    result = runner.invoke(main,
+                           ['--config', config_file, 'galera', 'register']
+                           )
+    assert result.exit_code == 0
+    result = runner.invoke(main,
+                           ['--config', config_file, 'galera', 'server', 'set_offline', percona_xtradb_cluster_three_node[0]['ip'], '3306']
+                           )
+    assert result.exit_code == 0
+
+    result = runner.invoke(main,
+                           ['--config', config_file, 'galera', 'register']
+                           )
+    assert result.exit_code == 0
+
+    connection = pymysql.connect(
+        host=proxysql_instance.host,
+        port=proxysql_instance.port,
+        user=proxysql_instance.user,
+        passwd=proxysql_instance.password,
+        connect_timeout=20,
+        cursorclass=DictCursor)
+
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute('SELECT `hostgroup_id`, `hostname`, '
+                           '`port`, `status`, `weight`, `compression`, '
+                           '`max_connections`, `max_replication_lag`, '
+                           '`use_ssl`, `max_latency_ms`, `comment`'
+                           ' FROM `mysql_servers`'
+                           ' WHERE hostgroup_id = %s'
+                           ' AND hostname = %s',
+                           (
+                               hostgroup_writer,
+                               percona_xtradb_cluster_three_node[0]['ip']
+                           )
+                           )
+            row = cursor.fetchall()[0]
+            assert row['status'] == BackendStatus.offline_hard
+    finally:
+        connection.close()
+
+
+def test__galera_server_set_offline_writer_if_backends_are_registered_with_empty_mysql_servers(percona_xtradb_cluster_three_node,
+                                                                                               proxysql_instance,
+                                                                                               tmpdir):
+
+    wait_for_cluster_nodes_to_become_healthy(percona_xtradb_cluster_three_node)
+    hostgroup_writer = 10
+    hostgroup_reader = 11
 
     blacklist = '{}:3306'.format(percona_xtradb_cluster_three_node[1]['ip'])
     nodes = [
