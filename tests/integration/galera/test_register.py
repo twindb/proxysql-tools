3e151612a78116c28fe3fe82f0466e58fb9ed432
mkryva <maksym.kryva@gmail.com>
2017-08-29 13:16:40 +0300

    Fix tests

--- tests/integration/galera/test_register.py
+++ tests/integration/galera/test_register.py
@@ -272,6 +272,20 @@ def test__galera_register_sync_desync_state(percona_xtradb_cluster_three_node,
     hostgroup_writer = 10
     hostgroup_reader = 11
 
+    rw_map = {
+        0: hostgroup_writer,
+        1: hostgroup_reader,
+        2: hostgroup_reader
+    }
+    for i in xrange(3):
+        backend = ProxySQLMySQLBackend(
+            hostname=percona_xtradb_cluster_three_node[i]['ip'],
+            port=percona_xtradb_cluster_three_node[0]['mysql_port'],
+            hostgroup_id=rw_map[i]
+        )
+        proxysql_instance.register_backend(backend)
+
+
     desync_node = GaleraNode(
         host=percona_xtradb_cluster_three_node[1]['ip'],
         port=percona_xtradb_cluster_three_node[1]['mysql_port'],
@@ -365,6 +379,20 @@ def test__galera_register_writer_node_is_reader_when_readers_list_is_empty(perco
     wait_for_cluster_nodes_to_become_healthy(percona_xtradb_cluster_three_node)
     hostgroup_writer = 10
     hostgroup_reader = 11
+
+
+    rw_map = {
+        0: hostgroup_writer,
+        1: hostgroup_reader,
+        2: hostgroup_reader
+    }
+    for i in xrange(3):
+        backend = ProxySQLMySQLBackend(
+            hostname=percona_xtradb_cluster_three_node[i]['ip'],
+            port=percona_xtradb_cluster_three_node[0]['mysql_port'],
+            hostgroup_id=rw_map[i]
+        )
+        proxysql_instance.register_backend(backend)
 
     desync_node = GaleraNode(
         host=percona_xtradb_cluster_three_node[1]['ip'],
